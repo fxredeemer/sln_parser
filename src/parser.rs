39d73a9c5f14f93a7a623409631afdd92036794a
Flurin Feuerstein <flufeu@gmail.com>
2022-07-01 21:02:43 +0200

    parsed configurations

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use crate::structures::{GeneralInformation, Project, ProjectDraft, Solution};
+use crate::structures::{GlobalInformation, Header, Project, ProjectDraft, Solution};
 use regex::Regex;
 use std::vec;
 use uuid::Uuid;
@@ -16,15 +16,14 @@ impl Parser {
     }
 
     pub fn parse_solution_file(&self, content: String) -> Result<Solution, String> {
-        let (general_information, remainder) = parse_general_information(&content)?;
+        let (header, remainder) = parse_general_information(&content)?;
         let (projects, remainder) = parse_projects(remainder);
-
-        println!("===========================");
-        //println!("{remainder}");
+        let global_information = parse_global_information(remainder)?;
 
         Ok(Solution {
-            general_information,
+            header,
             projects,
+            global_information,
         })
     }
 }
@@ -32,10 +31,10 @@ impl Parser {
 fn parse_projects(mut remainder: &str) -> (Vec<Project>, &str) {
     let mut projects = vec![];
 
-    let project_start = format!("EndProject{}", LINE_ENDING);
+    let start_tag = format!("EndProject{}", LINE_ENDING);
 
-    while let Some(end_project) = remainder.find(&project_start) {
-        let (project_str, remainder_str) = remainder.split_at(end_project + project_start.len());
+    while let Some(end_project) = remainder.find(&start_tag) {
+        let (project_str, remainder_str) = remainder.split_at(end_project + start_tag.len());
         remainder = remainder_str;
         if let Some(project) = parse_project(project_str) {
             projects.push(project);
@@ -110,13 +109,56 @@ fn map_projects(projects: &Vec<ProjectDraft>) -> Vec<Project> {
             path: draft.path.clone(),
             project_type: draft.project_type,
             dependencies,
+            configurations: vec![],
         };
         out_projects.push(project);
     }
     out_projects
 }
 
-fn parse_general_information(content: &str) -> Result<(GeneralInformation, &str), String> {
+fn parse_global_information(data: &str) -> Result<GlobalInformation, String> {
+    let global_part = get_global_part(data)?;
+    let solution_configuration_data = get_solution_configuration_part(global_part)?;
+
+    let mut solution_configurations = vec![];
+    let lines = solution_configuration_data.split(LINE_ENDING);
+    
+    let regex = Regex::new(r#"(\w+\|\w+) = \w+\|\w+"#).unwrap();
+    for line in lines {
+        if let Some(captures) = regex.captures_iter(line).next(){
+            solution_configurations.push(captures[1].to_owned());
+        }
+    }
+
+    Ok(GlobalInformation {
+        solution_configurations,
+    })
+}
+
+fn get_solution_configuration_part(global_part: &str) -> Result<&str, String> {
+    let section_start_tag = "GlobalSection(SolutionConfigurationPlatforms)";
+    let section_end_tag = "EndGlobalSection";
+    let section_start = global_part
+        .find(&section_start_tag)
+        .ok_or("Unable to find Start of Global Section SolutionConfigurationPlatforms")?;
+    let data = &global_part[section_start..];
+    let section_end = data
+        .find(&section_end_tag)
+        .ok_or("Unable to find End of Global Section SolutionConfigurationPlatforms")?;
+    Ok(&global_part[..section_end])
+}
+
+fn get_global_part(data: &str) -> Result<&str, String> {
+    let start_tag = format!("Global{}", LINE_ENDING);
+    let end_tag = format!("EndGlobal{}", LINE_ENDING);
+    let start = data
+        .find(&start_tag)
+        .ok_or("Unable to find Start of Global")?;
+    let end = data.find(&end_tag).ok_or("Unable to find End of Global")?;
+    Ok(&data[start..end])
+}
+
+fn parse_general_information(content: &str) -> Result<(Header, &str), String> {
     let start_project = content
         .find("Project(")
         .ok_or("Could not read first Project")?;
@@ -124,7 +166,7 @@ fn parse_general_information(content: &str) -> Result<(GeneralInformation, &str)
     println!("{general_information}");
 
     Ok((
-        GeneralInformation {
+        Header {
             visual_studio_version: general_information[..1].to_owned(),
             minimum_visual_studio_version: general_information[..1].to_owned(),
         },
--- src/structures.rs
+++ src/structures.rs
@@ -4,12 +4,18 @@ use serde::Serialize;
 
 #[derive(Debug, Serialize)]
 pub struct Solution {
+    pub header: Header,
     pub projects: Vec<Project>,
-    pub general_information: GeneralInformation,
+    pub global_information: GlobalInformation
 }
 
 #[derive(Debug, Serialize)]
-pub struct GeneralInformation {
+pub struct GlobalInformation{
+    pub solution_configurations: Vec<String>
+}
+
+#[derive(Debug, Serialize)]
+pub struct Header {
     pub visual_studio_version: String,
     pub minimum_visual_studio_version: String,
 }
@@ -21,6 +27,7 @@ pub struct Project {
     pub path: String,
     pub project_type: Uuid,
     pub dependencies: Vec<String>,
+    pub configurations: Vec<String>
 }
 
 #[derive(Debug)]
