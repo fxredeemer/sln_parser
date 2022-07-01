397f2a467b82b2bc4961448793fcd3df51184975
Flurin Feuerstein <flufeu@gmail.com>
2022-07-01 21:55:33 +0200

    added file writing

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -1,18 +1,29 @@
 use std::fs;
 
-pub struct FileReader {
-    path: String,
+pub struct FileHandler {
+    in_path: String,
+    out_path: String,
 }
 
-impl FileReader {
-    pub fn new(path: String) -> Self {
-        Self { path }
+impl FileHandler {
+    pub fn new(in_path: &str, out_path: &str) -> Self {
+        Self {
+            in_path: in_path.to_owned(),
+            out_path: out_path.to_owned(),
+        }
     }
 
     pub fn get_contents(&self) -> Result<String, String> {
-        match fs::read_to_string(&self.path) {
+        match fs::read_to_string(&self.in_path) {
             Ok(content) => Ok(content),
             Err(_) => Err("Unable to read file contents".to_owned()),
+        }
+    }
+
+    pub fn save_to_file(&self, data: String) -> Result<(), String> {
+        match fs::write(&self.out_path, data) {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Unable to write file".to_owned()),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use file_reader::FileReader;
+use file_reader::FileHandler;
 use parser::Parser;
 
 mod file_reader;
@@ -6,14 +6,14 @@ mod parser;
 mod structures;
 
 fn main() -> Result<(), String> {
-    let file_reader = FileReader::new("samples/Files.sln".to_owned());
+    let file_handler = FileHandler::new("samples/Files.sln", "samples/files.json");
     let parser = Parser::new();
 
-    let content = file_reader.get_contents()?;
+    let content = file_handler.get_contents()?;
     let solution = parser.parse_solution_file(content)?;
 
     let serialized = serde_json::to_string_pretty(&solution).unwrap();
-    println!("{serialized}");
+    file_handler.save_to_file(serialized)?;
 
     Ok(())
 }
